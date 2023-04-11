6c6487b0d2fc7f9fdc0c0f8098c483c661c7ad5e
R051299 <rashedrazzak77@gmail.com>
2023-04-11 15:39:54 +0600

    about section

--- owl.js
+++ owl.js
@@ -25,3 +25,55 @@ $(function () {
     });
 
 });
+$(function () {
+    $('#sponsor-slider-active').owlCarousel({
+        margin: 20,
+        nav: true,
+        items: 5,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            768: {
+                items: 3,
+                nav: false,
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: true,
+            },
+        },
+
+    });
+
+});
+$(function () {
+    $('#team-mate-slider-active').owlCarousel({
+        margin: 20,
+        nav: true,
+        items: 4,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            768: {
+                items: 2,
+                nav: false,
+            },
+            1000: {
+                items: 4,
+                nav: true,
+                loop: true,
+            },
+        },
+
+    });
+
+});
